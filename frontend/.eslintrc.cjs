dbb7505f84b73ca05061787fae58a25bf54befc1
Fiodorova Maria <mariya.sg.rp@gmail.com>
2026-04-28 21:17:39 -0500

    infra: add airbnb config for linter

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-refresh'],
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'prettier'
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+        {
+          devDependencies: [
+            '**/*.test.ts',
+            '**/*.test.tsx',
+            '**/*.spec.ts',
+            '**/*.spec.tsx',
+            'vite.config.ts',
+            '*.config.ts',
+          ],
+        },
+    ],
+  }
+}
