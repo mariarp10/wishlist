31065a27a0ba5f4eec15d68f03d9dfa58cd0ee95
Fiodorova Maria <mariya.sg.rp@gmail.com>
2026-04-28 20:03:42 -0500

    feat: add ui input

---
+++ frontend/src/components/ui/input/input.tsx
@@ -0,0 +1,56 @@
+import styles from './input.module.css'
+import { useState, type InputHTMLAttributes } from 'react'
+import EyeOpen from '@icons/eye-open.svg?react'
+import EyeClosed from '@icons/eye-closed.svg?react'
+
+type UIInputProps = {
+  extraClass?: string
+  label?: string
+  extraInputClass?: string
+  errorMessage?: string
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export const UIInput = ({ id, extraClass = '', label = '', extraInputClass = '', errorMessage, type = "text", ...rest }: UIInputProps) => {
+  const [passwordOpen, setPasswordOpen] = useState(false);
+
+  const customType = type === "password" ? (passwordOpen ? "text" : "password") : type;
+
+  const Icon = passwordOpen ? EyeOpen : EyeClosed;
+
+  const handleTogglePassword = () => {setPasswordOpen((prev) => !prev)}
+
+  return (
+    <div className={`${styles.content} ${extraClass}`}>
+      {label && (
+        <label
+          htmlFor={id}
+          className={`text text_type_main text_color_black mb-4 ${styles.label}`}
+        >
+          {label}
+        </label>
+      )}
+      <input
+        {...rest}
+        id={id}
+        type={customType}
+        className={`${styles.input} text text_type_main text_color_primary ${extraInputClass}`}
+      />
+      {type === "password" && (
+        <button
+          type="button"
+          className={styles.eye_btn}
+          onClick={handleTogglePassword}
+        >
+          <Icon />
+        </button>
+      )}
+      {errorMessage && (
+        <span
+          className={`text text_type_main text_color_red mt-4 ${styles.error}`}
+        >
+          {errorMessage}
+        </span>
+      )}
+    </div>
+  )
+}
